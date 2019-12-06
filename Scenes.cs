0af275ad44ab7776557fabf262e34444af20503d
ygorm <55883592+ygorm@users.noreply.github.com>
2019-12-06 08:38:12 -0300

    Add files via upload

---
+++ Scenes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Scenes : MonoBehaviour {
+	
+	 public float targetTime = 10.0f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetTime -= Time.deltaTime;
+ 
+ 		if (targetTime <= 0.0f)
+ 		{
+			 SceneManager.LoadScene (1);
+	 	}
+	}
+
+}
